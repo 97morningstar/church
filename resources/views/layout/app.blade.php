4f3bf201fc334039b2defca8b6898ec474926a66
Elisa Martinez <97morningstar@gmail.com>
2020-01-19 02:28:47 -0600

    Added Icon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -46,9 +46,9 @@
 
         <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/fontello/css/fontello.css" type="text/css" media="all" />
-        <link rel="icon" href="http://placehold.it/32x32" />
-        <link rel="icon" href="http://placehold.it/32x32" />
-        <link rel="apple-touch-icon-precomposed" href="http://placehold.it/32x32" />
+        <link rel="icon" type="image/ico"  href="favicon.ico" />
+       
+     
         <link rel="stylesheet" href="css/alll.css" type="text/css" media="all" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
